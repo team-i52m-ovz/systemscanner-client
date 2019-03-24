b73234d4ff56522bc2bce617576ebe74e0f104c6
Serg Vasylchak <hustle3942@gmail.com>
2019-03-25 00:14:03 +0200

    Update Angular version to 7+

--- src/app/services/auth-data-service.ts
+++ src/app/services/auth-data-service.ts
@@ -3,22 +3,21 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { CommonHttp } from '../models/constants/constants';
 import { map } from 'rxjs/operators';
-import { Router } from '@angular/router';
 import { environment } from '../../environments/environment';
 
 @Injectable()
 export class AuthDataService {
   private api: string;
 
-  constructor(private httpClient: HttpClient, private router: Router) {
+  constructor(private httpClient: HttpClient) {
     this.api = environment.scannerApi;
   }
 
   login(user: { username: string, password: string }): Observable<any> {
     return this.httpClient.post<HttpResponse<any>>(`${this.api}auth/login`, user, {observe: 'response'})
       .pipe(
-        map((res: HttpHeaderResponse) =>
-          localStorage.setItem(CommonHttp.headers.auth, res.headers.get(CommonHttp.headers.auth)))
+        map(res =>
+          localStorage.setItem(CommonHttp.headers .auth, res.headers.get(CommonHttp.headers.auth)))
       );
   }
 }
